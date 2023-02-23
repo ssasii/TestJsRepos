765e73b82d6be515e0f74ced8de57c8f62b5fccb
ssasii <121158430+ssasii@users.noreply.github.com>
2023-02-23 12:32:18 +0530

    Update React.js

--- React.js
+++ React.js
@@ -18,3 +18,30 @@ const [userProfiles, setProfiles] = React.useState([]);
   };
   
   // use filterDatas to the UI layer
+
+//usecallback
+
+import React, { memo, useCallback } from 'react';
+import './style.css';
+
+export default function App() {
+  const [count, setCount] = React.useState(0);
+
+  const increment = useCallback(() => {
+    setCount(Math.floor(Math.random() * 100));
+  }, []);
+
+  return (
+    <div>
+      count is {count}
+      <br />
+      <Increment increment={increment} />
+    </div>
+  );
+}
+
+const Increment = memo(({ increment }) => {
+  console.log('increment is getting called');
+  return <button onClick={increment}>Increment</button>;
+});
+
