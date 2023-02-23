c7cd6adb7a8b5f1f18f3ca25d3c2cb002a8aa9ad
ssasii <121158430+ssasii@users.noreply.github.com>
2023-02-23 12:44:30 +0530

    Update React.js

--- React.js
+++ React.js
@@ -45,3 +45,33 @@ const Increment = memo(({ increment }) => {
   return <button onClick={increment}>Increment</button>;
 });
 
+
+//custom hooks:
+import React, { useState, useEffect } from 'react';
+import './style.css';
+
+export default function App() {
+  const data = useFetch(`https://jsonplaceholder.typicode.com/todos`);
+
+  return (
+    <div>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </div>
+  );
+}
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [url]);
+
+  return data;
+};
+
